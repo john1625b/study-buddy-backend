f51dea8b623440779a36042fc5d8dd791e56349c
John <john1625b@gmail.com>
2020-05-05 18:27:53 -0400

    adds alternate port

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const { Client } = require('pg');
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT;
-console.log('port used:', port)
+const port = process.env.PORT || 3000;
 
 const client = new Client({
   connectionString: 'postgres://aunyheipusmsan:9991e0c9017992e076140123aa1c9eb306be4505e3e4c051109be62c4227a85e@ec2-3-234-109-123.compute-1.amazonaws.com:5432/d7t6af5r5aip0g',
@@ -17,6 +16,7 @@ addRow = "insert into study_groups (course, assignment, location) values ('calc'
 get_all_query = 'select * from study_groups';
 
 function get_table() {
+  
   client.query(get_all_query, (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
